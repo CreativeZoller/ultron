0b1ba85b82e037080585a01d38b4bff9ce546038
CreativeZoller <zoltan.belicza@gmail.com>
2016-03-22 14:13:35 +0100

    tests fixed, main javascript changed for better workflow in the future

--- src/tests/mySpec.js
+++ src/tests/mySpec.js
@@ -1,17 +1,22 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe('JavaScript addition operator', function () {
+  it('adds two numbers together', function () {
+    expect(1 + 2).toEqual(3);
+  });
+});
+describe('JavaScript variable', function () {
+  it('is not defined', function () {
+    var name;
+    expect(name).toBeUndefined();
   });
 });
 
-describe('JavaScript addition operator', function () {
-    it('adds two numbers together', function () {
-        expect(1 + 2).toEqual(3);
-    });
+describe('testFunctionOne', function () {
+  it('should return a * b', function () {
+    expect(testFunctionOne(2,4)).toEqual(8);
+  });
 });
-describe('JavaScript variable', function () {
-	it("is not defined", function () {
-	    var name;
-	    expect(name).toBeUndefined();
-	});
+describe("sqrt", function() {
+  it("should compute the square root of 4 as 2", function() {
+    expect(My.sqrt(4)).toEqual(2);
+  });
 });
